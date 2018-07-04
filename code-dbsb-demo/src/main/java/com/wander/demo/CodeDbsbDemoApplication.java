43705198a16eaed928096f3af9856b1e0095070f
wanderhero <cengjingsea@163.com>
2018-07-04 17:23:04 +0800

    模块整理

---
+++ code-dbsb-demo/src/main/java/com/wander/demo/CodeDbsbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wander.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDbsbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDbsbDemoApplication.class, args);
+    }
+
+}
---
+++ code-dbsb-demo/src/test/java/com/wander/demo/CodeDbsbDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wander.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeDbsbDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
