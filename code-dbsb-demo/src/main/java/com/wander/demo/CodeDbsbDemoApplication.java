812339bbf40c7bcba8d5ab78a49df7a2602d596a
wanderhero <cengjingsea@163.com>
2018-07-06 14:02:34 +0800

    连接数据库

--- code-dbsb-demo/src/main/java/com/wander/demo/CodeDbsbDemoApplication.java
+++ code-dbsb-demo/src/main/java/com/wander/demo/CodeDbsbDemoApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @desc CodeDbsbDemoApplication
  */
 @SpringBootApplication
-@MapperScan("com.wander.demo.dao")
 public class CodeDbsbDemoApplication {
 
     public static void main(String[] args) {
