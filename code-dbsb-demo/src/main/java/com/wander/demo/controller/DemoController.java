e04fd8c83d41629b968ee806b604a811509291ee
wanderhero <cengjingsea@163.com>
2018-07-15 21:49:41 +0800

    测试提交到多个 git

--- code-dbsb-demo/src/main/java/com/wander/demo/controller/DemoController.java
+++ code-dbsb-demo/src/main/java/com/wander/demo/controller/DemoController.java
@@ -29,7 +29,7 @@ public class DemoController {
     @GetMapping("/test")
     public String test() {
         String test = demoService.test();
-        logger.error("test6");
+        logger.error("test");
         return "hello : " + test;
     }
 
