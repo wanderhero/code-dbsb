188a02032ba57a2799fcb0b15c9847599e00d363
wanderhero <cengjingsea@163.com>
2018-07-25 00:00:52 +0800

    添加 redis 配置

--- code-dbsb-demo/src/main/java/com/wander/demo/service/impl/DemoServiceImpl.java
+++ code-dbsb-demo/src/main/java/com/wander/demo/service/impl/DemoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.wander.demo.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wander.demo.dao.DemoDao;
 import com.wander.demo.entity.Demo;
 import com.wander.demo.service.DemoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,17 +19,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class DemoServiceImpl implements DemoService {
 
     private final DemoDao demoDao;
+    private final StringRedisTemplate redisTemplate;
 
     @Autowired
-    public DemoServiceImpl(DemoDao demoDao) {
+    public DemoServiceImpl(StringRedisTemplate redisTemplate, DemoDao demoDao) {
+        this.redisTemplate = redisTemplate;
         this.demoDao = demoDao;
     }
 
     @Transactional(rollbackFor = Exception.class)
     @Override
     public String test() {
-        // test(1)
-        Demo demo = demoDao.test2(1L);
+        Demo demo;
+        String demoJson = redisTemplate.opsForValue().get("demo");
+        if (StringUtils.isNotBlank(demoJson)) {
+            demo = JSONObject.parseObject(demoJson, Demo.class);
+        } else {
+            // test(1)
+            demo = demoDao.test2(1L);
+            if (null != demo) {
+                redisTemplate.opsForValue().set("demo", JSONObject.toJSONString(demo));
+            }
+        }
         if (null != demo) {
             demoDao.update(demo);
             return demo.getName();
