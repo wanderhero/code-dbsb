c894457a178afb909500dd7b947cf4aa9ebce92d
wanderhero <cengjingsea@163.com>
2018-10-21 22:14:26 +0800

    添加测试代码

---
+++ code-dbsb-demo/src/main/java/com/wander/demo/test/DateTest.java
@@ -0,0 +1,17 @@
+package com.wander.demo.test;
+
+import java.util.Date;
+
+/**
+ * @author Wander.Zeng
+ * @create 2018-10-18 16:07
+ * @desc DateTest
+ */
+public class DateTest {
+
+    public static void main(String[] args) {
+        System.out.println(new Date().getTime());
+        System.out.println(System.currentTimeMillis());
+    }
+
+}
